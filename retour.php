28e5be79c084809fb3b2551d65eb304b2e11a3bb
GradiM <gradi.mandambu@outlook.com>
2017-09-19 22:55:18 +0200

    Add files via upload

---
+++ retour.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Retour</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/modern-business.css" rel="stylesheet">
+
+         <!-- My CSS -->
+        <link href="css/css.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+        <?php include "menu/menu.php"; ?>
+
+        <!-- Page Content -->
+        <div class="container">
+
+        	<!-- Page Heading/Breadcrumbs -->
+        	<h1 id="retour_title"><small>Nos</small>
+		    	<span style="display: inline-block;" class="mt-4 mb-3">Retours</span>
+		    </h1>
+
+		    <ol class="breadcrumb">
+		        <li class="breadcrumb-item">
+		          <a href="index.html">Acceuil</a>
+		        </li>
+		        <li class="breadcrumb-item active">Retour</li>
+		    </ol>
+
+      		<!-- Retour's blocks -->
+
+            <div class="main_block">
+            	<div class="img_block1">
+	            	<img src="img/image1.png"></img>
+	            	<img class="second_img" src="img/image1.png"></img>
+            	</div>
+            	<div class="img_block2">
+	                <img src="img/image1.png"></img>
+	                <img class="mental_card" src="img/carte_mentale.png"></img>
+	                <img src="img/image1.png"></img>
+            	</div>
+            	<div class="img_block3">
+	                <img src="img/image1.png"></img>
+	                <img class="second_img" src="img/image1.png"></img>
+            	</div>
+
+            	<div class="vid_block1">
+	            	<img class="first_vid" src="img/image6.png"></img>
+	            	<img class="second_vid" src="img/image6.png"></img>
+	            	<img src="img/image6.png"></img>
+	            </div>
+	            <div class="vid_block2">
+	            	<img class="first_vid" src="img/image6.png"></img>
+	            	<img class="second_vid" src="img/image6.png"></img>
+	            	<img src="img/image6.png"></img>
+	            </div>
+            </div>
+        </div>
+    </body>
