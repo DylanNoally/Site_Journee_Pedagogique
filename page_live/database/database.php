2b32b87b4e3ed4b0d026324f1bd5ae93373686df
DylanNoally <dylan.window@hotmail.fr>
2017-09-29 12:47:14 +0200

    Delete database.php

--- page_live/database/database.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$bdd = new PDO('mysql:host=localhost;dbname=ppe;charset=utf8', 'root', ''); 
-
-try {
-$con = new PDO('mysql:host=localhost;dbname=ppe', 'root', '');;
-} catch(Exeption $e) {
-	die($e);
-}
-?>
