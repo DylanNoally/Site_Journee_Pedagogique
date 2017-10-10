ebb8f0abb252258b8e789ad3f9780e91aaf3cb44
DylanNoally <dylan.window@hotmail.fr>
2017-10-10 12:40:04 +0200

    Header du Site

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Journée Pédagogique | Lycée Saint Vincent</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="css/monfichier.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div class="containerTop">
+      <a href="http://www.lycee-stvincent.fr/"><img class="img-fluid rounded mb-4" src="logo.png" alt="" align="center"></a>
+      <h1 class="top-title">Créer, partager, construire ensemble</h1>
+      <h2 class="under-title">Journée pédagogique</h2>
+    </div>
+    <?php
+      session_start();
+      include 'menu.php';
+    ?>
+
